e0579bb65f3456a5277d1262f1bf0a620689fcb9
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2021-02-15 20:47:51 +0700

    if data null

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -4,17 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Data;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\Cast\Object_;
 
 class DataController extends Controller
 {
     public function index()
     {
         $data = Data::find(1);
+        if( is_null($data) ){
+            $temp = array([
+                "start" => "",
+                "end" => "",
+                "note" => ""
+            ]);
+            $data = (object) $temp[0];
+            // dd(gettype($data));
+            // return $data;
+        }
         return view('welcome', compact('data'));
     }
 
     public function store(Request $request)
     {
+        return $request;
         $data = Data::find(1);
 
         if(is_null($data)){
